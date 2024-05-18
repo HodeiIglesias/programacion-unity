13c775c2daa679650e3f8a09daa437c076d68be0
HodeiIglesias <hodeiiglesiasibarrondo2009@gmail.com>
2024-05-18 15:19:12 +0200

    Add files via upload

---
+++ Crickets.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crickets : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Grillo por frame
+    void Update()
+    {
+        print("crickets");
+    }
+}
---
+++ Destroy.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour
+{
+    // destrozar la capsula
+    void Start()
+    {
+        Destroy(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
